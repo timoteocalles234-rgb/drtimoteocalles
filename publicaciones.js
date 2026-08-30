b4d4beb7c4f130ac3cf6b3d2e91db4f1097e2462
timoteocalles234-rgb <timoteocalles234@gmail.com>
2026-08-30 18:49:41 -0300

    Update publicaciones.js

--- publicaciones.js
+++ publicaciones.js
@@ -31,7 +31,7 @@ async function cargarPublicaciones(area, contenedorId) {
         console.log("ÁREA BUSCADA:", area);
 
         const lista = publicaciones.filter(p =>
-            p.estado === "publicada" &&
+            
             p.area === area
         );
 
